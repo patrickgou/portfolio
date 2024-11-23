a605a6eb5d702fcf70c701470aee471bd0d73046
patrickgou <goulartpatrick5@gmail.com>
2024-11-23 18:30:12 -0300

    upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    //'./components/**/*.{html,js}',
+    './src/**/*.{html,js}',
+    './index.html',
+  ],
   theme: {
     extend: {},
   },
